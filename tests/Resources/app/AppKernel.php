93fd2df2e56fbf9656a20fcef251da6aa7d551a1
dantleech <dan.t.leech@gmail.com>
2014-06-03 11:49:33 +0200

    Fixed composer

--- tests/Resources/app/AppKernel.php
+++ tests/Resources/app/AppKernel.php
@@ -2,20 +2,23 @@
 
 use Symfony\Cmf\Component\Testing\HttpKernel\TestKernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
-class AppKernel extends TestKernel
+class AppKernel extends Kernel
 {
-    public function configure()
+    public function registerBundles()
     {
-        $this->requireBundleSets(array(
-            'default', 'phpcr_odm'
-        ));
-
-        $this->addBundles(array(
+        return array(
             new \Symfony\Cmf\Bundle\RoutingBundle\CmfRoutingBundle(),
             new \Slinp\Bundle\SlinpBundle\SlinpBundle(),
             new \Slinp\Bundle\SlinpTestBundle\SlinpTestBundle(),
-        ));
+            new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new \Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new \Symfony\Bundle\TwigBundle\TwigBundle(),
+            new \Symfony\Bundle\MonologBundle\MonologBundle(),
+            new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
+            new \Doctrine\Bundle\PHPCRBundle\DoctrinePHPCRBundle(),
+        );
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
